33a7415a07284a83069f218177ff2b83661557a6
Chibuike Ewenike <sopewenike@yahoo.com>
2023-10-28 14:43:09 +0100

    added break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xxxs: "400px",
+      s450: "450px",
+      xxs: "500px",
+      xs: "540px",
+      s: "600px",
+      sm: "640px",
+      ssm: "670px",
+      m: "700px",
+      md: "768px",
+      mmd: "800px",
+      mmmd: "850px",
+      pc: "992px",
+      lg: "1024px",
+      llg: "1124px",
+      xl: "1200px",
+      xxl: "1536px",
+    },
     extend: {
       animation: {
         ping: "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite",
