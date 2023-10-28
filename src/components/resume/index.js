e3df92fa45dfe2d754cc60b33b8f75eb4313ba90
Chibuike Ewenike <sopewenike@yahoo.com>
2023-10-28 20:33:18 +0100

    resume finished

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -1,30 +1,87 @@
 import Image from "next/image";
 import React, { useRef } from "react";
 import useOnScreen from "../hooks/hooks";
-
+import { FcCalendar } from "react-icons/fc";
 function Resume({ setIntersecting, isvisible }) {
   // { isIntersecting, setIntersecting }
   const resumeref = useRef(null);
   useOnScreen(resumeref, setIntersecting);
   //   console.log("intersectingresume", isvisible);
+  const resumeDetails = [
+    {
+      title: "Front End Developer",
+      date: "2020 - 2021",
+      type: "Freelance",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit incididunt ut labore et.",
+      position: "justify-start",
+    },
+    {
+      title: "Marketing Strategist",
+      date: "2020 - 2022",
+      type: "Freelance",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit incididunt ut labore et.",
+      position: "justify-end",
+    },
+    {
+      title: "SEO Developer",
+      date: "2021 - 2021",
+      type: "Freelance",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit incididunt ut labore et.",
+      position: "justify-start",
+    },
+    {
+      title: "Back End Developer",
+      date: "2022 - 2023",
+      type: "Freelance",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit incididunt ut labore et.",
+      position: "justify-end",
+    },
+  ];
   return (
     <section
       ref={resumeref}
       id="resume"
-      className="h-screen text-white relative mt-[30px]"
+      className="h-auto text-white relative mt-[30px]"
     >
       <div className="">
         <h3 className="mt-[30px] text-[#f0f0f0] underAbout relative inline-block text-[20px] md:text-[24px] lg:text-[31px] font-semibold tracking-[.3px] leading-[normal] mb-[40px]">
           MY RESUME
         </h3>
         <div className="pt-[40px] relative ">
-          <div className=" timeline h-[700px]">
-            <div className="dot relative grid grid-cols-2">
-              <div className="mb-[35px]">
-                <h2 className="text-center">Full Stack Developer</h2>
-                <div className="flex">
-                  a <h4>2020 - 2021</h4> | <h3>Freelance</h3>
+          <div className=" timeline h- [700px]">
+            {resumeDetails.map((item) => (
+              <div
+                key={item.title}
+                className={`dot w-full ${item.position} flex relative`}
+              >
+                <div className="mb-[35px] text-[#c7c5c7] w-[47%] border-[#374757] border-[3px] shadow-[0px_4px_5px_rgba(37,50,66,.7)] m-[3px]">
+                  <h2 className="text-center text-[#16c0f0] py-[15px] font-semibold tracking[.5px] text-base">
+                    {item.title}
+                  </h2>
+                  <h3 className="text-center relative text-[13px] mx-[20px] mb-[15px] border-b-[#373757] border-b pb-[15px]">
+                    <span>
+                      <FcCalendar size={12} className="inline mb-[3px]" />{" "}
+                      {item.date}
+                    </span>{" "}
+                    | <span>{item.type}</span>
+                  </h3>
+                  <p className="text-[14px] px-[20px] pb-[15px] leading-[1.8rem] font-normal font-open_Sans ">
+                    {item.details}
+                  </p>
                 </div>
+              </div>
+            ))}
+            {/* <div className="dot relative justify-end flex w-full">
+              <div className="mb-[35px]  w-[47%] ">
+                <h2 className="text-center">Front End Developer</h2>
+                <h3 className="text-center">
+                  <FcCalendar size={15} /> <span>2020 - 2021</span> |{" "}
+                  <span>Freelance</span>
+                </h3>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
                   do eiusm tempor incididunt ut labore et dolore magna aliqua
@@ -32,7 +89,7 @@ function Resume({ setIntersecting, isvisible }) {
                   incididunt ut labore et.
                 </p>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
